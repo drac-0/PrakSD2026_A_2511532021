166aff00d092eeaf0b293c2cd98f3bf3b282fda3
Muhammad faiz <muhammadfaiz050607@gmail.com>
2026-03-31 07:05:59 +0700

    Praktikum pekan 1 2511532021

--- src/pekan1_2511532021/Jam_2511532021.java
+++ src/pekan1_2511532021/Jam_2511532021.java
@@ -29,8 +29,48 @@ public class Jam_2511532021 {
 	public void setMinute(int m) {
 		this.mm= m;
 	}
-	public void setHour(int s) {
+	public void setSecond(int s) {
 		this.ss = s;
 	}
-
+	
+	public int toSeconds() {
+		return hh * 3600 + mm * 60 + ss;
+	}
+	public static Jam_2511532021 fromSecond(int Total) {
+		if (Total < 0) throw new IllegalArgumentException("Detik Negatif");
+		Total %= 24 * 3600;
+		int h = Total / 3600; Total %= 3600;
+		int m = Total /60 ; int s = Total % 60;
+		return new Jam_2511532021(h,m,s);
+	}
+	
+	public int compareTo(Jam_2511532021 other) {
+		return Integer.compare(this.toSeconds(), other.toSeconds());
+	}
+	
+	public boolean equals(Object o) {
+		if (!(o instanceof Jam_2511532021 j)) return false;
+		return hh == j.hh && mm ==  j.mm && ss == j.ss;
+	}
+	
+	public int hashCode() {
+		return java.util.Objects.hash(hh,mm,ss);
+	}
+	
+	public Jam_2511532021 plus(Jam_2511532021 Other) { return fromSecond(this.toSeconds() + Other.toSeconds());}
+	public Jam_2511532021 minus(Jam_2511532021 Other) { return fromSecond(Math.floorMod(this.toSeconds(), Other.toSeconds()));}
+	public Jam_2511532021 nextSecond(){ return fromSecond(this.toSeconds() + 1);}
+	public Jam_2511532021 nextNsecond(int N) {return fromSecond(this.toSeconds() + Math.max(0, N));}
+	public Jam_2511532021 prevSecond() { return fromSecond(Math.floorMod(this.toSeconds() -1 , 24 * 3600 ));} 
+	public Jam_2511532021 prevNsecond(int N) {return fromSecond(Math.floorMod(this.toSeconds() - Math.max(0, N), 24*3600));}
+	
+	public static int durasiDetik(Jam_2511532021 jaw, Jam_2511532021 jakh) {
+		return jakh.toSeconds() - jaw.toSeconds();
+	}
+	
+	public String toString() {
+		return String.format("%02d:%02d:%02d ", hh,mm,ss);
+	}
+	
+	
 }
